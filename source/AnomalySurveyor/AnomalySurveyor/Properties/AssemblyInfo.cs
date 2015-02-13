33677eafb4b62bb812d1bfd5d6e1a5a96b202bc9
Jeremie Rossignol <jrossignol@gmail.com>
2015-02-12 21:32:55 -0700

    1.0.1

--- source/AnomalySurveyor/AnomalySurveyor/Properties/AssemblyInfo.cs
+++ source/AnomalySurveyor/AnomalySurveyor/Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
 
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 [assembly: KSPAssembly("AnomalySurveyor", 1, 0)]
 [assembly: KSPAssemblyDependency("ContractConfigurator", 1, 0)]
